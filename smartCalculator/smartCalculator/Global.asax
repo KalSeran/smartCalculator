38740fbbfc79932bcda0347c0c34758859b8c990
Aztek <kkamil411x@gmail.com>
2016-04-19 19:18:17 +0200

    Pierwszy push

---
+++ smartCalculator/smartCalculator/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="smartCalculator.MvcApplication" Language="C#" %>
---
+++ smartCalculator/smartCalculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(smartCalculator.Startup))]
+namespace smartCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
